5a583263d0ef3f2f93b9bb3a31185d6009708908
e-montenegro <eliseomontenegro16@gmail.com>
2023-09-29 21:51:48 -0300

    se actualiza enlace
    se actualiza enlace

--- instituto/Adman/lista_planes.php
+++ instituto/Adman/lista_planes.php
@@ -1,6 +1,6 @@
 <?php
-require_once './ModelsPlan/crearPlan.php';
-require_once 'includes/header.php';
+require_once './MpdelsPlan/crearPlan.php';
+require_once './includes/header.php';
 require_once '../Includes/load.php';
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
